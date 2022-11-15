f93c08d0c40a34c8d775d4e677a8e4628cd0a4bf
Quy Nguyen <tanquyvt@gmail.com>
2022-11-15 23:26:54 +0700

    update script install dependencies

--- shell/install_dependencies.sh
+++ shell/install_dependencies.sh
@@ -4,6 +4,6 @@ sudo apt update
 # Ensure support for apt repositories served via HTTPS
 sudo apt install apt-transport-https
 
-sudo apt-add-repository universe
+sudo apt-add-repository -y universe
 
 sudo apt update
